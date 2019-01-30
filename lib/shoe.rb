c2babf71e59a893aaa86ecb29d89be698f428c38
Dylan Michael Labat <dml252@live.com>
2019-01-30 22:57:18 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,14 @@
 class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
   
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def brand
+    @brand
+  end
+  
   def cobble
     puts ""
     @condition = "new"
